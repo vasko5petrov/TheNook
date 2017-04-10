dfeb09da0d39cf41312e6edf6b72286cf1d6b926
Vasko Petrov <vasko5petrov@gmail.com>
2017-04-10 17:08:01 +0300

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,5 @@
 <?php 
-mysql_connect("127.0.0.1", "root", "qawsedrf") or die (mysql_error());
+mysql_connect("127.0.0.1", "root", "") or die (mysql_error());
 mysql_select_db("videosocial")or die(mysql_error());
 session_start();
 ?>
